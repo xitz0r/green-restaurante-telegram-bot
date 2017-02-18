909e0de8cf20d3b444a20efe7a58dc770e8b5859
Bruno Paulo <bruno@werules.com>
2017-02-18 21:35:47 -0200

    initial commit

---
+++ soup.py
@@ -0,0 +1,11 @@
+from lxml import html
+import requests
+
+
+def get_soup():
+    page = requests.get("http://www.greenrestaurante.com.br/our-menu/")
+    tree = html.fromstring(page.content)
+    menu_items = tree.xpath('//section[@class="menu-item"]')
+    for item in menu_items:
+        if item.xpath('span/strong/text()')[0] == 'SOPA':
+            return item.xpath('span[@class="description"]/text()')
