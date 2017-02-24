6e0d4ebd3f26813ae72ddca50a3bccc1d3cc9f98
Bruno Paulo <bruno@werules.com>
2017-02-24 20:43:11 -0300

    returning also the daily juice

--- soup.py
+++ soup.py
@@ -2,10 +2,20 @@ from lxml import html
 import requests
 
 
-def get_soup():
+def get_soup_and_juice():
     page = requests.get("http://www.greenrestaurante.com.br/our-menu/")
     tree = html.fromstring(page.content)
     menu_items = tree.xpath('//section[@class="menu-item"]')
+    soup = ''
+    juice = ''
+
     for item in menu_items:
         if item.xpath('span/strong/text()')[0] == 'SOPA':
-            return item.xpath('span[@class="description"]/text()')[0]
+            soup = item.xpath('span[@class="description"]/text()')[0]
+
+    menu_items = tree.xpath('//div[@class="col-md-10 col-md-offset-1 menu-category-group"]')
+    for item in menu_items:
+        if item.xpath('h2/text()')[0] == 'Refresco do dia ':
+            juice = item.xpath('section/span[@class="title"]/strong/text()')[0]
+
+    return soup, juice
